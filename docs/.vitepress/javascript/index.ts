0370010364f4177de20db961a153acf3049915e5
lixj <1497404080@qq.com>
2023-12-19 15:08:02 +0800

    fix：Js垃圾回收机制篇bug修复

--- docs/.vitepress/javascript/index.ts
+++ docs/.vitepress/javascript/index.ts
@@ -1,5 +1,5 @@
 export default [
   { text: "JavaScript基础", link: "/JavaScript/Js基础" },
-  { text: "JS垃圾回收机制", link: "/JavaScript/JS垃圾回收机制" },
+  { text: "JS垃圾回收机制", link: "/JavaScript/Js垃圾回收机制" },
   { text: "迭代器与生成器", link: "/JavaScript/迭代器与生成器" },
 ];
