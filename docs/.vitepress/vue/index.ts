9334c6fcc2014e5870b89b849c90e8ac87901b1d
lixj <1497404080@qq.com>
2023-12-19 20:28:40 +0800

    deploy

--- docs/.vitepress/vue/index.ts
+++
@@ -1,6 +0,0 @@
-export default [
-  { text: "Vue2响应式原理", link: "/Vue/Vue2响应式原理" },
-  { text: "深入虚拟DOM", link: "/Vue/虚拟DOM" },
-  { text: "重温computed", link: "/Vue/重温computed" },
-  { text: "Vue3响应式原理", link: "/Vue/Vue3响应式原理" },
-];
