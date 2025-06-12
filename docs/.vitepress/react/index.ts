8c454cfd7de1e13329885dddd828d707a1592b9f
lixj <lxj1497404080@gmail.com>
2025-06-12 22:49:32 +0800

    ✨ feat: React-fiber

--- docs/.vitepress/react/index.ts
+++ docs/.vitepress/react/index.ts
@@ -2,4 +2,5 @@ export default [
   { text: "React Hooks", link: "/React/React_hooks.md" },
   { text: "React并发模式", link: "/React/React并发模式.md" },
   { text: "React运行机制", link: "/React/React运行机制.md" },
+  { text: "React Fiber", link: "/React/React_fiber.md" },
 ];
